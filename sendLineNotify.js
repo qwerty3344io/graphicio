752d13545820291d8afed73c09c6a8716e294acf
qwerty3344io <otingotingm@gmail.com>
2025-03-14 11:27:09 +0700

    Create sendLineNotify.js

---
+++ sendLineNotify.js
@@ -0,0 +1,18 @@
+exports.handler = async function(event, context) {
+  const accessToken = "YOUR_ACCESS_TOKEN";
+  const message = "📢 แจ้งเตือนจาก Netlify Functions!";
+
+  const response = await fetch("https://notify-api.line.me/api/notify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": `Bearer ${accessToken}`,
+    },
+    body: new URLSearchParams({ message: message }),
+  });
+
+  return {
+    statusCode: response.status,
+    body: JSON.stringify(await response.json()),
+  };
+};
